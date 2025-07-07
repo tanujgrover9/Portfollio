c4fa81857b84c779f4708fa55d268ca3abe4de6c
tanujgrover9 <tanujgrover07@gmail.com>
2025-07-07 19:53:11 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+"use client"; // optional: forces all rendering on client
+
 import { Approach } from "@/components/approach";
 // import { Clients } from "@/components/clients";
 import { Experience } from "@/components/experience";
@@ -13,11 +15,9 @@ const MainPage = () => {
   return (
     <main className="relative mx-auto flex flex-col items-center justify-center overflow-clip bg-black-100 px-5 sm:px-10">
       <FloatingNav navItems={navItems} />
-
       <div className="w-full max-w-7xl">
         <Hero />
-       
-        <TechStackSection/>
+        <TechStackSection />
         <Grid />
         <RecentProjects />
         {/* <Clients /> */}
